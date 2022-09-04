5475ace4ad0b8cd1514c5d540b9f268b30cde459
Vanshaj <vanshajdhar@gmail.com>
2022-09-04 19:11:34 +0530

    Created a new core adapter which implements the port interface

---
+++ internal/adapters/core/arithmetic/arithmetic.go
@@ -0,0 +1,29 @@
+package arithmetic
+
+/*
+	First adapter that will match our port interface created
+*/
+
+// We have created our own adapter type. All the adapters we create will have this Adapter type
+type Adapter struct {
+}
+
+func NewAdapter() *Adapter {
+	return &Adapter{}
+}
+
+func (arith Adapter) Addition(a int32, b int32) (int32, error) {
+	return a + b, nil
+}
+
+func (arith Adapter) Subtraction(a int32, b int32) (int32, error) {
+	return a - b, nil
+}
+
+func (arith Adapter) Multiplication(a int32, b int32) (int32, error) {
+	return a * b, nil
+}
+
+func (arith Adapter) Division(a int32, b int32) (int32, error) {
+	return a / b, nil
+}
