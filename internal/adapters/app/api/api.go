9501c2b7173d3b6f69aa63f05e259d68922925e9
Vanshaj <vanshajdhar@gmail.com>
2022-09-04 20:11:15 +0530

    MAJOR-Our application layer must have access to frameworks db port, hence encompassing the same

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -5,36 +5,41 @@ import "github.com/vanshaj/HexArch/internal/ports"
 /*
 	Now we are in the application layer
 	If we think of hex architecture , application layer encompasses the core layer. Hence composition.
-	So our application layer has a core layer's port ( Composition not inheritance)
+	So our application layer has a core layer's port and framework layers port too( Composition not inheritance)
 */
 
 type Adapter struct {
 	arith ports.ArithmeticPort
+	db    ports.DbPort
 }
 
-// Dependency injection of ports's Arithmetic port
-func NewAdapter(arith ports.ArithmeticPort) *Adapter {
-	return &Adapter{arith}
+// Dependency injection of ports's Arithmetic port and frameworks db port
+func NewAdapter(arith ports.ArithmeticPort, db ports.DbPort) *Adapter {
+	return &Adapter{arith, db}
 }
 
 // Implements all methods of application layer ports
 
 func (apia Adapter) GetAddition(a, b int32) (int32, error) {
 	answer, err := apia.arith.Addition(a, b)
+	err = apia.db.AddToHistory(answer, "addition")
 	return answer, err
 }
 
 func (apia Adapter) GetSubtraction(a, b int32) (int32, error) {
 	answer, err := apia.arith.Subtraction(a, b)
+	err = apia.db.AddToHistory(answer, "subtraction")
 	return answer, err
 }
 
 func (apia Adapter) GetDivision(a, b int32) (int32, error) {
 	answer, err := apia.arith.Division(a, b)
+	err = apia.db.AddToHistory(answer, "division")
 	return answer, err
 }
 
 func (apia Adapter) GetMultiplication(a, b int32) (int32, error) {
 	answer, err := apia.arith.Multiplication(a, b)
+	err = apia.db.AddToHistory(answer, "multiplication")
 	return answer, err
 }
